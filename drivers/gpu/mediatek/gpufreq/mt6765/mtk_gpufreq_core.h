46bd4ac3cb2e1a5a821da8c312e1b2c3727d4365
Rissu J <rissu.ntk@gmail.com>
2024-12-30 07:36:35 +0000

    TREEWIDE: Major changes
    Signed-off-by: Rissu J <rissu.ntk@gmail.com>

--- drivers/gpu/mediatek/gpufreq/mt6765/mtk_gpufreq_core.h
+++ drivers/gpu/mediatek/gpufreq/mt6765/mtk_gpufreq_core.h
@@ -33,6 +33,27 @@
 /**************************************************
  * MT6765 segment_3 : GPU DVFS OPP table Setting
  **************************************************/
+#ifdef CONFIG_MISC_OPT_OVERCLOCK
+#define SEG3_GPU_DVFS_FREQ0			(800000)	/* KHz */
+#define SEG3_GPU_DVFS_FREQ1			(700000)	/* KHz */
+#define SEG3_GPU_DVFS_FREQ2			(680000)	/* KHz */
+#define SEG3_GPU_DVFS_FREQ3			(500000)	/* KHz */
+#define SEG3_GPU_DVFS_FREQ4			(400000)	/* KHz */
+
+#define SEG3_GPU_DVFS_VOLT0			(85200)		/* mV x 100 */
+#define SEG3_GPU_DVFS_VOLT1			(83000)		/* mV x 100 */
+#define SEG3_GPU_DVFS_VOLT2			(80000)		/* mV x 100 */
+#define SEG3_GPU_DVFS_VOLT3			(70000)		/* mV x 100 */
+#define SEG3_GPU_DVFS_VOLT4			(65000)		/* mV x 100 */
+
+#define SEG3_GPU_DVFS_VSRAM0			(87500)		/* mV x 100 */
+#define SEG3_GPU_DVFS_VSRAM1			(87500)		/* mV x 100 */
+#define SEG3_GPU_DVFS_VSRAM2			(87500)		/* mV x 100 */
+#define SEG3_GPU_DVFS_VSRAM3			(87500)		/* mV x 100 */
+#define SEG3_GPU_DVFS_VSRAM4			(87500)		/* mV x 100 */
+
+#else /* !CONFIG_MISC_OPT_OVERCLOCK */
+
 #define SEG3_GPU_DVFS_FREQ0			(680000)	/* KHz */
 #define SEG3_GPU_DVFS_FREQ1			(500000)	/* KHz */
 #define SEG3_GPU_DVFS_FREQ2			(400000)	/* KHz */
@@ -44,6 +65,7 @@
 #define SEG3_GPU_DVFS_VSRAM0			(87500)		/* mV x 100 */
 #define SEG3_GPU_DVFS_VSRAM1			(87500)		/* mV x 100 */
 #define SEG3_GPU_DVFS_VSRAM2			(87500)		/* mV x 100 */
+#endif
 
 /**************************************************
  * MT6765T segment_4 : GPU DVFS OPP table Setting
